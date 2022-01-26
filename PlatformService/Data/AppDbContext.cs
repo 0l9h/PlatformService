dcb2e7ee43c86a1c23842077c1f81965ec6bd330
Oleh <olehbobeliak@gmail.com>
2022-01-26 23:24:31 +0200

    Add project files.

---
+++ PlatformService/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PlatformService.Models;
+
+namespace PlatformService.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> opts) : base(opts) {}
+
+        public DbSet<Platform>? Platforms { get; set; }
+    }
+}
